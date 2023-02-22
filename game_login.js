6fce540b4e408088330d1fd9cced2f8b1b9d29b6
BeastBane24 <112552319+BeastBane24@users.noreply.github.com>
2023-02-22 18:29:35 +0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+// Create function addUser()
+function addUser()
+{
+player1_name = document.getElementById("player1_input").value;
+player2_name = document.getElementById("player2_input").value;
+localStorage.setItem("player1_input", player1_name);
+localStorage.setItem("player2_input", player2_name);
+window.location ="game_page.html";
+
+}
+
